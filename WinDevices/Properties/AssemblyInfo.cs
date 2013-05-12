24ed21997c9cdc5245d800412bb419214dcbff61
Aleksander Polak <apolak@gmail.com>
2013-05-12 19:32:51 +0200

    Add Visual Studio project stub

---
+++ WinDevices/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WinDevices")]
+[assembly: AssemblyProduct("WinDevices")]
+[assembly: AssemblyCopyright("Copyright © 2013 Aleksander Polak")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
