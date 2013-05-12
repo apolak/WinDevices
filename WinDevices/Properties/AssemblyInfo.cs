ae6106bd82432c77a6e6469186195f130d5d4243
Aleksander Polak <apolak@gmail.com>
2013-05-12 20:05:50 +0200

    Add license and copyright notice

--- WinDevices/Properties/AssemblyInfo.cs
+++ WinDevices/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("WinDevices")]
 [assembly: AssemblyProduct("WinDevices")]
-[assembly: AssemblyCopyright("Copyright © 2013 Aleksander Polak")]
+[assembly: AssemblyCopyright("Copyright © 2013 Aleksander Polak. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
