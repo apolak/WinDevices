7e4ed5b289af72cecda1e7c980da0b7e8e34604f
Aleksander Polak <apolak@gmail.com>
2013-05-12 20:33:56 +0200

    Add device information set creation and disposal interop types

---
+++ WinDevices/Interop/NativeMethods.cs
@@ -0,0 +1,63 @@
+﻿namespace WinDevices.Interop
+{
+   using System;
+   using System.Runtime.InteropServices;
+
+   [Flags]
+   internal enum SetupDiGetClassDevsEx_Flags
+   {
+      None = 0,
+      DIGCF_DEFAULT = 0x00000001,
+      DIGCF_PRESENT = 0x00000002,
+      DIGCF_ALLCLASSES = 0x00000004,
+      DIGCF_PROFILE = 0x00000008,
+      DIGCF_DEVICEINTERFACE = 0x00000010,
+   }
+
+   internal static class NativeMethods
+   {
+      [DllImport("setupapi", CharSet = CharSet.Auto, SetLastError = true)]
+      public static extern SafeDeviceInfoSetHandle SetupDiGetClassDevsEx(
+         ref Guid ClassGuid,
+         string Enumerator,
+         IntPtr hwndParent,
+         SetupDiGetClassDevsEx_Flags Flags,
+         IntPtr DeviceInfoSet,
+         string MachineName,
+         IntPtr Reserved);
+
+      [DllImport("setupapi", CharSet = CharSet.Auto, SetLastError = true)]
+      public static extern SafeDeviceInfoSetHandle SetupDiGetClassDevsEx(
+         IntPtr ClassGuid,
+         string Enumerator,
+         IntPtr hwndParent,
+         SetupDiGetClassDevsEx_Flags Flags,
+         IntPtr DeviceInfoSet,
+         string MachineName,
+         IntPtr Reserved);
+
+      [DllImport("setupapi", CharSet = CharSet.Auto, SetLastError = true)]
+      public static extern IntPtr SetupDiGetClassDevsEx(
+         ref Guid ClassGuid,
+         string Enumerator,
+         IntPtr hwndParent,
+         SetupDiGetClassDevsEx_Flags Flags,
+         SafeDeviceInfoSetHandle DeviceInfoSet,
+         string MachineName,
+         IntPtr Reserved);
+
+      [DllImport("setupapi", CharSet = CharSet.Auto, SetLastError = true)]
+      public static extern IntPtr SetupDiGetClassDevsEx(
+         IntPtr ClassGuid,
+         string Enumerator,
+         IntPtr hwndParent,
+         SetupDiGetClassDevsEx_Flags Flags,
+         SafeDeviceInfoSetHandle DeviceInfoSet,
+         string MachineName,
+         IntPtr Reserved);
+
+      [DllImport("setupapi", SetLastError = true)]
+      [return: MarshalAs(UnmanagedType.Bool)]
+      public static extern bool SetupDiDestroyDeviceInfoList(IntPtr DeviceInfoSet);
+   }
+}
---
+++ WinDevices/Interop/SafeDeviceInfoSetHandle.cs
@@ -0,0 +1,24 @@
+﻿namespace WinDevices.Interop
+{
+   using System;
+   using Microsoft.Win32.SafeHandles;
+
+   public sealed class SafeDeviceInfoSetHandle : SafeHandleZeroOrMinusOneIsInvalid
+   {
+      private SafeDeviceInfoSetHandle()
+         : base(true)
+      {
+      }
+
+      public SafeDeviceInfoSetHandle(IntPtr handle, bool ownsHandle)
+         : base(ownsHandle)
+      {
+         this.SetHandle(handle);
+      }
+
+      protected override bool ReleaseHandle()
+      {
+         return NativeMethods.SetupDiDestroyDeviceInfoList(this.handle);
+      }
+   }
+}
